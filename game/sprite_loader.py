c6faada82e9821e3ae023de50ef55ae61423f993
dasha <study.code@mail.ru>
2025-12-14 12:34:16 +0300

    инструмент для отражения кадров

--- game/sprite_loader.py
+++ game/sprite_loader.py
@@ -16,3 +16,12 @@ class SpriteLoader:
         # для всех файлов, если не указан размер, то он останется изначальным. Если указан, то он будет менять размер на указанный
         return sprite
 
+    #функция для отражения каждого спрайта !!!инструмент!!!
+    def flip_sprites(sprites, flip_x=False, flip_y=False):
+        return [pygame.transform.flip(sprite, flip_x, flip_y) for sprite in sprites]
+    #transform - модуль для преобразования, flip - отразить
+    #анимация бега влево = [
+        #кадр1 бег влево,   отражённая версия
+        #кадр2 бег влево,... отражённая версия
+    # ]
+
