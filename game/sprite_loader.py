616bf5a325d7ff825fadc9e6fa49441eecab82db
Anastasiya Simonenko <Simonenkoa2005@gmail.com>
2025-12-16 03:17:40 +0300

    Возвращение sprite_loader.py

--- game/sprite_loader.py
+++ game/sprite_loader.py
@@ -1,42 +1,33 @@
 import pygame
 import os
 
+
 class SpriteLoader:
     def load_sprite(path, width=None, height=None):
-        #path - путь к файлу изображения
-        sprite = pygame.image.load(path) #атрибут для хранения изображения
+        sprite = pygame.image.load(path)
 
-        if sprite.get_alpha() is None: #если файл прозрачный возвращает 255, если нет 0. Если непрозрачный
-            sprite = sprite.convert() #конвертируем без прозрачности
-
-        else: #если файл имеет прозрачность
-            sprite = sprite.convert_alpha() # конвентируем как прозрачный
+        if sprite.get_alpha() is None:
+            sprite = sprite.convert()
+        else:
+            sprite = sprite.convert_alpha()
 
         if width and height:
             sprite = pygame.transform.scale(sprite, (width, height))
-        # для всех файлов, если не указан размер, то он останется изначальным. Если указан, то он будет менять размер на указанный
+
         return sprite
 
     load_sprite = staticmethod(load_sprite)
 
-    #функция для отражения каждого спрайта !!!инструмент!!!
+    #функция для отражения каждого спрайта
     def flip_sprites(sprites, flip_x=False, flip_y=False):
         return [pygame.transform.flip(sprite, flip_x, flip_y) for sprite in sprites]
-    #transform - модуль для преобразования, flip - отразить
-    #анимация бега влево = [
-        #кадр1 бег влево,   отражённая версия
-        #кадр2 бег влево,... отражённая версия
-    # ]
+
     flip_sprites = staticmethod(flip_sprites)
 
-    #инструмент для получения пути
     def get_assets_path():
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        #os.path.dirname получаем папку, где лежит файл - "C:\project\game\run.py" → "C:\project\game\"
-        #получаем путь и делаем его абсолютым(от корня до фала) - "run.py" → "C:\project\game\run.py"
+        assets_path = os.path.join(current_dir, '..', 'assets')
 
-        assets_path = os.path.join(current_dir, '..', 'assets') #выходим в родительску папку и получаем путь с assets
-        #os.path.join("C:\project\game\", "..", "assets") → "C:\project\assets"
         return assets_path
 
     get_assets_path = staticmethod(get_assets_path)
